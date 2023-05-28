61c9a74505f02a5dab5b9d8de20cf6388c4c1568
Michał Jaros <13064089+michal-jar@users.noreply.github.com>
2023-05-28 21:21:29 +0200

    feat: export all auto craft receipes

--- src/export/fortrescraft.py
+++ src/export/fortrescraft.py
@@ -81,6 +81,9 @@ def extract_items(game_data: Path, id_converter: IdConverter) -> Iterator[Tuple[
             "stack": 200 if item_entry.findtext('Type') == "ItemStack" else 1
         }
         yield key.lower() if key else object_id, object, category
+        object_key = item_entry.findtext('Object')
+        if object_key and object_key != key:
+            yield object_key.lower() if key else object_id, object, category
 
 
 def extract_recipes(receipe_set: Path, receipe_set_id: str, factory_id: str, machine_name: str, all_items: Dict) -> Iterator[Tuple[Dict, List]]:
@@ -111,6 +114,54 @@ def extract_recipes(receipe_set: Path, receipe_set_id: str, factory_id: str, mac
             yield receip, used_items
 
 
+def extract_auto_recipes(receipe: Path, all_items: Dict) -> Iterator[Tuple[Dict, Dict, List]]:
+    auto_craft = ET.parse(receipe).getroot()
+    factory_id = auto_craft.findtext("Value")
+    if factory_id:
+        factory = all_items[factory_id.lower()]
+        factory["factory"] = { "speed": 1, "type": "electric", "usage": 4000 }
+        for craft in auto_craft.findall("Recipe"):
+            key = craft.findtext("CraftedKey", default=craft.findtext("Key"))
+            if key:
+                crafted_object = all_items.get(key.lower().replace(" ", ""))
+                if not crafted_object:
+                    print(f'Missing crafted item: {receipe} = {key}')
+                    continue
+                costs = dict()
+                used_items = [factory, crafted_object]
+                for cost in craft.findall("Costs/CraftCost"):
+                    key = cost.findtext("Key", default=cost.findtext("Name"))
+                    if key:
+                        object = all_items.get(key.lower().replace(" ", "").replace("-", ""))
+                        if not object:
+                            print(f'Missing ingridient for: {receipe} = {key}')
+                            continue
+                        used_items.append(object)
+                        costs[object["id"]] = int(cost.findtext("Amount", default=0))
+                    else:
+                        print(f'Missing ingridient for: {receipe}')
+                craft_costs = [{cost: amount} for cost, amount in costs.items()] if craft.findtext("OptionalIngredients", default="false").lower() == "true" else [costs]
+                craft_time = float(auto_craft.findtext("CraftTime", default=0))
+                craft_energy_cost = craft_time * float(auto_craft.findtext("PowerUsePerSecond", default=0))
+                crafted_amount = int(craft.findtext("CraftedAmount", default=0))
+                for receipe_cost in craft_costs:
+                    if not receipe_cost:
+                        print(f'Craft without ingridients: {receipe}')
+                        continue
+                    receip = {
+                        "id": f'{crafted_object["id"]}-{factory["id"]}',
+                        "name": f'{crafted_object["name"]} ({factory["name"]})',
+                        "cost": craft_energy_cost,
+                        "time": craft_time,
+                        "in": receipe_cost,
+                        "out": { crafted_object["id"]: crafted_amount },
+                        "producers": [factory["id"]],
+                        "row": 0,
+                        "category": crafted_object["category"]
+                    }
+                    yield factory, receip, used_items
+
+
 def icons_configuration() -> Dict[str, Tuple[int, int]]:
     icons_config_path = Path(__file__).parent / 'icons_fill.json'
     with icons_config_path.open() as icons_config_file:
@@ -138,6 +189,11 @@ def main():
         all_items[key] = item
         categories[category["id"]] = category
 
+    all_items["coal"] = all_items.get("coalore")
+    all_items["coalenrichment"] = all_items.get("enrichedcoal")
+    all_items["fuelcanisterkey"] = all_items.get("emptyfuelcanister")
+    all_items["chargedplasmahead"] = all_items.get("plasmacutterhead")
+
     used_items = dict()
     data_recipes = dict()
     factories = dict()
@@ -162,6 +218,14 @@ def main():
                         for object in items:
                             used_items[object["id"]] = object
 
+    for auto_craft_receipe in (game_data / "GenericAutoCrafter").iterdir():
+        if auto_craft_receipe.is_file():
+            for factory, receip, items in extract_auto_recipes(auto_craft_receipe, all_items):
+                factories[factory["id"]] = factory
+                data_recipes[receip["id"]] = receip
+                for object in items:
+                    used_items[object["id"]] = object
+
     icons_config = icons_configuration()
     icons_image, icons_positions = game_icons(game_data.parent.parent / 'FC_Linux_Universal_Data', icons_config)
     icons_image.save(Path(__file__).parent / 'icons.png')
@@ -174,7 +238,7 @@ def main():
         },
         "icons": factoriolab_icons
     }
-    write(Path(__file__).parent / 'icons.json', factoriolab_icons, True)
+    write(Path(__file__).parent / 'icons.json', factoriolab_icons, False)
 
     no_icon_position = icons_positions.get("no-icon")
     factoriolab_data = {
@@ -184,14 +248,14 @@ def main():
         "items": list(used_items.values()),
         "recipes": list(data_recipes.values())
     }
-    write(Path(__file__).parent / 'data.json', factoriolab_data, True)
+    write(Path(__file__).parent / 'data.json', factoriolab_data, False)
 
     factoriolab_hash = {
         "items": list(used_items.keys()),
         "factories": list(factories.keys()),
         "recipes": list(data_recipes.keys())
     }
-    write(Path(__file__).parent / 'hash.json', factoriolab_hash, True)
+    write(Path(__file__).parent / 'hash.json', factoriolab_hash, False)
 
 
 if __name__ == '__main__':
